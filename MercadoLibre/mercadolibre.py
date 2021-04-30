f38db84ef4d571b859e6769fe9df86c5dddc9f18
Nestor Villafañe Mass <mass.nestor@gmail.com>
2021-04-30 16:27:06 -0500

    Modulo mercadolibre

---
+++ MercadoLibre/mercadolibre.py
@@ -0,0 +1,19 @@
+import  requests
+from bs4 import BeautifulSoup
+    
+def buscar_producto():
+    url = "https://listado.mercadolibre.com.co/portatil-vostro-dell-3400#D[A:portatil%20vostro%20dell%203400]"
+
+    r = requests.get(url)
+    print(r.status_code)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # articulos = soup.find('ol', class_ ='ui-search-layout ui-search-layout--stack')
+    articulos = soup.find_all('li', class_ ='ui-search-layout__item')
+    # print(articulos)
+    for articulo in articulos:
+        nombre = articulo.find('h2', class_='ui-search-item__title').text
+        precioreal = articulo.find('span', class_='price-tag-fraction').text
+        precio = articulo.find('span', class_='price-tag ui-search-price__part').text
+        
+        print(f"Nombre: {nombre} \tprecio: {precio}")
--- run.py
+++ run.py
@@ -1,31 +1,6 @@
-import  requests
-from bs4 import BeautifulSoup
-
+from MercadoLibre.mercadolibre import buscar_producto
 def main():
-    url = "https://listado.mercadolibre.com.co/portatil-vostro-dell-3400#D[A:portatil%20vostro%20dell%203400]"
-
-    r = requests.get(url)
-    print(r.status_code)
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    # articulos = soup.find('ol', class_ ='ui-search-layout ui-search-layout--stack')
-    articulos = soup.find_all('li', class_ ='ui-search-layout__item')
-    # print(articulos)
-    for articulo in articulos:
-        nombre = articulo.find('h2', class_='ui-search-item__title').text
-        precioreal = articulo.find('span', class_='price-tag-fraction').text
-        precio = articulo.find('span', class_='price-tag ui-search-price__part').text
-        
-        print(f"Nombre: {nombre} \tprecio: {precio}")
-
-
-    # for articulo in articulos.find_all("div"):
-    #     # precios = articulo.find('spam',) 
-    #     print(articulo)
-    #     # contenido = articulo.find_all('h2')
-    #     # precios = articulo.find_all('div')
-    #     # for titulo in contenido:
-    #     #     print(titulo)
+    buscar_producto()
 
 
 if __name__ == '__main__':
